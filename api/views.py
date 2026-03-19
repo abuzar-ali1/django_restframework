3232f9a87e1d79a8988e87e16f79e6802a84ace5
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-03-19 16:41:35 +0500

    Get method using @api_view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.reponse import Response
+from .models import Student
+from .serializers import StudentSerializers
+
+
 
 # Create your views here.
+@api_view(['GET' ,'POST' , 'PUT' , 'DELETE' ])
+
+def StudentAPI(request):
+    if request.method == 'GET':
+        id = request.data.get(id)
+        if id is not None:
+            stu =  Student.objects.get(id=id)
+            seriliazer = StundetSerializer(stu)
+            return Response(serializer.data)
+
+    stu = Student.objects.all()
+    serializer = StundetSerializer(Stu)
+    return Reponse(serializer.data)        
