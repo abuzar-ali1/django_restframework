cc862b8e1368ca5a3a8eea843f2ba99b35bf3c75
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-03-24 20:55:28 +0500

    Testing the custom permissions on diff methods

--- api/permissions.py
+++ api/permissions.py
@@ -4,6 +4,6 @@ from rest_framework.permissions import BasePermission
 
 class StudentPermissions(BasePermission):
     def has_permission(self , request , view):
-        if request.method == 'GET':
+        if request.method == 'GET' or request.method == 'POST':
             return True
         else: return False    
